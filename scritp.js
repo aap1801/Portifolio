733c0d80e1209ed28767f1438174765702607530
aap1801 <aap2@cesar.school>
2025-12-09 14:05:27 -0300

    Criacao do repositorio meu portifolio

---
+++ scritp.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    const nav = document.getElementById("nav-menu");
+    nav.classList.toggle("active");
+}
+
+function toggleDarkMode() {
+    document.body.classList.toggle("dark");
+}
